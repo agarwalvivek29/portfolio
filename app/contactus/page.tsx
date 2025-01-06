4aafdd094e939d6c2b9ce5909d09814fb15c2ebb
Vivek Agarwal <agarwalvivek2902@gmail.com>
2025-01-07 03:10:14 +0530

    added contactus and terms-n-conditions

---
+++ app/contactus/page.tsx
@@ -0,0 +1,13 @@
+export default function ContactUs() {
+    return <div className="bg-black flex flex-col items-center justify-center text-white w-screen h-screen text-lg">
+        <div>
+            <h1 className="text-white font-semibold pb-2 text-2xl">Contact Us</h1>
+            <div>
+                <span className="font-bold">Name:</span> Vivek Agarwal<br/>
+                <span className="font-bold">Email:</span> agarwalvivek2902@gmail.com<br/>
+                <span className="font-bold">Phone:</span> +91 8748991090<br/>
+                <span className="font-bold">Address:</span> Bangalore, India
+            </div>
+        </div>
+    </div>
+}
